eadc08fc4a2611a04cd49caafd411fe43aba50e2
1m4mr4f1 <imamrafi192005@gmail.com>
2026-09-05 23:22:45 +0700

    feat: complete coding and ai page slicing and refactor mentor section

---
+++ src/data/mentors.ts
@@ -0,0 +1,32 @@
+export const mentors = [
+  {
+    image: "/farhan.webp",
+    major: "Biotechnology",
+    name: "Farhan M.",
+    university: "Institut Teknologi Bandung",
+  },
+  {
+    image: "/farhan.webp",
+    major: "Biotechnology",
+    name: "Farhan M.",
+    university: "Institut Teknologi Bandung",
+  },
+  {
+    image: "/farhan.webp",
+    major: "Biotechnology",
+    name: "Farhan M.",
+    university: "Institut Teknologi Bandung",
+  },
+  {
+    image: "/farhan.webp",
+    major: "Biotechnology",
+    name: "Farhan M.",
+    university: "Institut Teknologi Bandung",
+  },
+  {
+    image: "/farhan.webp",
+    major: "Biotechnology",
+    name: "Farhan M.",
+    university: "Institut Teknologi Bandung",
+  },
+];
